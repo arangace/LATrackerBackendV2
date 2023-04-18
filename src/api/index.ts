4337f2f4a7f46dd03c9d064fa0f61ea8214d62ee
AndyAceHuang <andy.huang@akqa.com>
2023-04-19 10:17:35 +1200

    testing api folder

---
+++ src/api/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { getDummyData } from "../controllers/userController";
+
+const router = express.Router();
+router.get("/", (req, res) => {
+  // Handle GET request for '/users' route
+  return res.send("users 🏓");
+});
+
+// router.get("/", getDummyData);
+
+export default router;
