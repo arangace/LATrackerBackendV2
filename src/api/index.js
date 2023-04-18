fe23dff34d051766a87be1b651383672025d6018
AndyAceHuang <andy.huang@akqa.com>
2023-04-19 11:19:27 +1200

    testing routes

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("express")();
-const { v4 } = require("uuid");
-
-app.get("/api", (req, res) => {
-  return res.send("users 🏓");
-});
-
-module.exports = app;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
 import usersRouter from "./routes/users";
+import api from "./api/index";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use("/users", usersRouter);
+app.use("/api", api);
 
 app.get("/", (_req: Request, res: Response) => {
   return res.send("Express Typescript on Vercel");
@@ -17,3 +19,4 @@ app.get("/ping", (_req: Request, res: Response) => {
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`);
 });
+export default app;
