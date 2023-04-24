a91b98c5b46f459583558f734d418bc5c80446dc
arangace <ahua291@aucklanduni.ac.nz>
2023-04-24 16:27:08 +1200

    removed account api completely

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -1,34 +1,34 @@
-import { Request, Response } from "express";
-import { userSchema } from "../models/user";
-import * as mongoDB from "mongodb";
-import * as dotenv from "dotenv";
+// import { Request, Response } from "express";
+// import { userSchema } from "../models/user";
+// import * as mongoDB from "mongodb";
+// import * as dotenv from "dotenv";
 
-export const getAccountData = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    dotenv.config();
+// export const getAccountData = async (
+//   req: Request,
+//   res: Response
+// ): Promise<void> => {
+//   try {
+//     dotenv.config();
 
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(
-      process.env.DATABASE_URL
-    );
+//     const client: mongoDB.MongoClient = new mongoDB.MongoClient(
+//       process.env.DATABASE_URL
+//     );
 
-    await client.connect();
-    const name = req.params.name;
-    const { testingName } = req.params;
+//     await client.connect();
+//     const name = req.params.name;
+//     const { testingName } = req.params;
 
-    // const db: mongoDB.Db = client.db(process.env.DB_NAME);
+//     // const db: mongoDB.Db = client.db(process.env.DB_NAME);
 
-    // const collection: mongoDB.Collection = db.collection(
-    //   process.env.COLLECTION_NAME
-    // );
-    // const data = await collection.findOne({ name: name });
+//     // const collection: mongoDB.Collection = db.collection(
+//     //   process.env.COLLECTION_NAME
+//     // );
+//     // const data = await collection.findOne({ name: name });
 
-    // console.log(data);
-    // client.close();
-    res.status(200).json(testingName + name);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
+//     // console.log(data);
+//     // client.close();
+//     res.status(200).json(testingName + name);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// };
