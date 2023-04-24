b7363b9622f7b0a49f9958f92d2637ef0f7c9c90
arangace <ahua291@aucklanduni.ac.nz>
2023-04-24 15:57:23 +1200

    added get all accounts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { getAccountData } from "../controllers/accountController";
+import { getAllAccounts } from "../controllers/getAllAccounts";
 
 const router = express.Router();
 
 router.use("/getAccount/:name", getAccountData);
+router.use("/", getAllAccounts);
 
 export default router;
